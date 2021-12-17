af3cb4e5a0bb9224ea3ac5119109a9189d3445de
Vinícius Silva <viniciussilvapereira24@gmail.com>
2021-12-17 14:28:20 -0300

    Clean Code

--- src/helpers/cepVerification.js
+++ src/helpers/cepVerification.js
@@ -3,8 +3,8 @@ const cep_avenida_das_cataratas = process.env.AVENIDA
 export async function cepVerification(cep_destino) {
     if (cep_destino === cep_avenida_das_cataratas) {
         console.log("ta aqui")
-        return 1;
+        return "1";
     } else {
-        return 0;
+        return "0";
     }
 }
