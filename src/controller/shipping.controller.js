d0dbad38abc918a27d29b8aff5f8218a42394ee6
Vinícius Silva <viniciussilvapereira24@gmail.com>
2021-12-17 14:20:04 -0300

    Clean Code

--- src/controller/shipping.controller.js
+++ src/controller/shipping.controller.js
@@ -29,7 +29,7 @@ class ShippingController {
       const shippingCost = calculateShippingCost(distance);
       
       const resultado = await cepVerification(cep_destino);
-      if(resultado !== 0){
+      if(resultado === 1){
         shippingCost = 10.99;
       }
 
