9fcd318c7290a8e3863b16adc1499637691cb5be
Vinícius Silva <viniciussilvapereira24@gmail.com>
2021-12-17 14:48:24 -0300

    Clean Code

--- src/controller/shipping.controller.js
+++ src/controller/shipping.controller.js
@@ -1,10 +1,9 @@
 import { getDistanceBetween } from "../helpers/getDistanceBetween.js";
 import { calculateShippingCost } from "../helpers/calculateShippingCost.js";
-import { cepVerification } from "../helpers/cepVerification.js";
 import axios from "axios";
 
 let tokenValue = process.env.TOKEN;
-
+let cep_avenida_das_cataratas = process.env.AVENIDA
 class ShippingController {
   async generateXML(request, response) {
     const { token, cep, cep_destino } = request.query;
@@ -14,71 +13,102 @@ class ShippingController {
 
     const origins = [cep.replace(/\D/g, "")];
     const destinations = [cep_destino.replace(/\D/g, "")];
+  
+    if (cep_destino === cep_avenida_das_cataratas) {
+      if (token === tokenValue){
+        const xml = `
+          <cotacao>
+            <resultado>         
+            <codigo></codigo>         
+            <transportadora>Pagar Online</transportadora>        
+            <servico>(Cartão de Crédito ou PIX)</servico>         
+            <transporte>Moto</transporte>         
+            <valor>10.99</valor>         
+            <peso></peso>         
+            <prazo_min>1 Minuto</prazo_min>         
+            <prazo_max>2 Horas</prazo_max>         
+            </resultado>
+          </cotacao>
+        `;
 
-    try {
-      let distance = 0;
-      try{
-        distance = await getDistanceBetween(origins, destinations);
-      } catch {
-        const result = await axios.get(`https://api.pagar.me/1/zipcodes/${cep_destino}`);
-        const newDestinations = `${result.data.street}, ${result.data.city}, ${result.data.zipcode}, ${result.data.state}`;
-        distance = await getDistanceBetween(origins, [newDestinations]);
+        return response.status(200).send(xml);
+      } else {
+        const xml = `
+          <cotacao>
+            <resultado>         
+            <codigo></codigo>         
+            <transportadora>Pagar na Entrega</transportadora>        
+            <servico>(Dinheiro ou Cartão)</servico>         
+            <transporte>Moto</transporte>         
+            <valor>10.99</valor>         
+            <peso></peso>         
+            <prazo_min>1 Minuto</prazo_min>         
+            <prazo_max>2 Horas</prazo_max>         
+            </resultado>
+          </cotacao>
+        `;
+
+        return response.status(200).send(xml);
       }
-      // distance = await getDistanceBetween(origins, destinations);
-      console.log(distance);
-      const shippingCost = calculateShippingCost(distance);
-      
-      const resultado = cepVerification(cep_destino);
-      if(resultado === 1) {
-        console.log(resultado)
-        const valorNovo = 10.99;
-        shippingCost = valorNovo;
-      }
-      if (shippingCost !== 0) {
-        if (token === tokenValue){
-          const xml = `
-            <cotacao>
-              <resultado>         
-              <codigo></codigo>         
-              <transportadora>Pagar Online</transportadora>        
-              <servico>(Cartão de Crédito ou PIX)</servico>         
-              <transporte>Moto</transporte>         
-              <valor>${shippingCost}</valor>         
-              <peso></peso>         
-              <prazo_min>1 Minuto</prazo_min>         
-              <prazo_max>2 Horas</prazo_max>         
-              </resultado>
-            </cotacao>
-          `;
-
-          return response.status(200).send(xml);
-        } else {
-          const xml = `
-            <cotacao>
-              <resultado>         
-              <codigo></codigo>         
-              <transportadora>Pagar na Entrega</transportadora>        
-              <servico>(Dinheiro ou Cartão)</servico>         
-              <transporte>Moto</transporte>         
-              <valor>${shippingCost}</valor>         
-              <peso></peso>         
-              <prazo_min>1 Minuto</prazo_min>         
-              <prazo_max>2 Horas</prazo_max>         
-              </resultado>
-            </cotacao>
-          `;
-
-          return response.status(200).send(xml);
+    } else {
+      try {
+        let distance = 0;
+        try{
+          distance = await getDistanceBetween(origins, destinations);
+        } catch {
+          const result = await axios.get(`https://api.pagar.me/1/zipcodes/${cep_destino}`);
+          const newDestinations = `${result.data.street}, ${result.data.city}, ${result.data.zipcode}, ${result.data.state}`;
+          distance = await getDistanceBetween(origins, [newDestinations]);
         }
-      }
+        // distance = await getDistanceBetween(origins, destinations);
+        console.log(distance);
+        const shippingCost = calculateShippingCost(distance);
 
-      return response.status(400).json({ message: "Distância excedeu o limite do delivery" });
-    } catch (error) {
-      // TODO: save error to file
-      return response.status(500).json({ message: "Erro interno no servidor, tente novamente" });
+        if (shippingCost !== 0) {
+          if (token === tokenValue){
+            const xml = `
+              <cotacao>
+                <resultado>         
+                <codigo></codigo>         
+                <transportadora>Pagar Online</transportadora>        
+                <servico>(Cartão de Crédito ou PIX)</servico>         
+                <transporte>Moto</transporte>         
+                <valor>${shippingCost}</valor>         
+                <peso></peso>         
+                <prazo_min>1 Minuto</prazo_min>         
+                <prazo_max>2 Horas</prazo_max>         
+                </resultado>
+              </cotacao>
+            `;
+
+            return response.status(200).send(xml);
+          } else {
+            const xml = `
+              <cotacao>
+                <resultado>         
+                <codigo></codigo>         
+                <transportadora>Pagar na Entrega</transportadora>        
+                <servico>(Dinheiro ou Cartão)</servico>         
+                <transporte>Moto</transporte>         
+                <valor>${shippingCost}</valor>         
+                <peso></peso>         
+                <prazo_min>1 Minuto</prazo_min>         
+                <prazo_max>2 Horas</prazo_max>         
+                </resultado>
+              </cotacao>
+            `;
+
+            return response.status(200).send(xml);
+          }
+        }
+
+        return response.status(400).json({ message: "Distância excedeu o limite do delivery" });
+      } catch (error) {
+        // TODO: save error to file
+        return response.status(500).json({ message: "Erro interno no servidor, tente novamente" });
+      }
     }
   }
-
 }
 
 export default new ShippingController();
