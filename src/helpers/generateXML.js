914c3e0b0a983932243bafb4d5454e7b40bd5835
Vinicius Silva Pereira <viniciussilvapereira24@gmail.com>
2022-02-10 11:50:42 -0300

    Removing the XML code from inside the Controller

---
+++ src/helpers/generateXML.js
@@ -0,0 +1,18 @@
+export function generateXML(data) {
+    console.log(data);
+    const xml = `
+        <cotacao>
+            <resultado>         
+            <codigo></codigo>         
+            <transportadora>Pagar Online</transportadora>        
+            <servico>(Cartão de Crédito ou PIX)</servico>         
+            <transporte>Moto</transporte>         
+            <valor>${data}</valor>         
+            <peso></peso>         
+            <prazo_min>1 Minuto</prazo_min>         
+            <prazo_max>2 Horas</prazo_max>         
+            </resultado>
+        </cotacao>
+    `
+    return xml;
+}
